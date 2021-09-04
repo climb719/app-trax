41495c6c0ae46660c3928227b456a1a4901a0cb7
climb719 <mary.oldweiler@gmail.com>
2021-09-04 14:42:18 -0400

    Job.js refector and start render methods

--- src/Job.js
+++ src/Job.js
@@ -8,13 +8,20 @@ class Job {
         console.log(this)
     }
 
-    static addJob(job) {
-        new Job(job)
+    static renderIndex() {
+    //     // create a job container and iterate through array of jobs
+    //     // call a method to render cards  
+
     }
+
 
     static getJobs() {
         api.getJobs().then(jobs => {
-            jobs.forEach(job => Job.addJob(job))
+            jobs.forEach(job => new Job(job))
+            //get all jobs, interating through them from the api, initializing each job
+            // then storing them in all array b/c constructor 
+            this.renderIndex()
+            
         })
     }
 }
