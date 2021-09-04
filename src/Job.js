377f44dae2f812fff6f17c48911bdd8481e722b1
climb719 <mary.oldweiler@gmail.com>
2021-09-04 13:59:13 -0400

    add constructor and static methods to instantiate jobs from fetch

--- src/Job.js
+++ src/Job.js
@@ -1,11 +1,18 @@
 class Job {
 
-    constructor(data) {
-        this.data = data
+
+    constructor(details) {
+        this.details = details
         console.log(this)
     }
 
+    static addJob(job) {
+        new Job(job)
+    }
+
     static getJobs() {
-        api.getJobs().then(console.log)
+        api.getJobs().then(jobs => {
+            jobs.forEach(job => Job.addJob(job))
+        })
     }
 }
