dc16fb878377ca4b89650c38c48e491b84164ee9
climb719 <mary.oldweiler@gmail.com>
2021-09-05 16:52:15 -0400

    test render options

--- src/Job.js
+++ src/Job.js
@@ -1,27 +1,32 @@
 class Job {
 
     static all = []
-
-    constructor(details) {
-        this.details = details
+  
+    constructor({id, title, company, notes, date, statusName}) {
+        this.id = id;
+        this.title = title;
+        this.company = company;
+        this.notes = notes;
+        this.date = date;
+        this.statusName = statusName
         this.constructor.all.push(this)
-        // console.log(this)
     }
 
     render = () => {
-        const {title, company, notes, date, link, id} = this.details
+        const {title, company, notes, date, link, statusName, id} = this
+        //debugger
         document.getElementById("job-container").innerHTML += `
         <div class="job-card" data-id=${id}>
         <p class="title">${title}</p>
         <p class="company">${company}</p>
         <p class="date">${date}</p>
         </div>`
-       
+      
     }
 
     static handleCardClick = (e) => {
        if (e.target.className === 'title') {
-           console.log(e.target)
+           //console.log(e.target)
        }
     }
    
@@ -30,15 +35,13 @@ class Job {
         const jobDiv = document.createElement('div')
         jobDiv.id = "job-container"
         document.getElementById('main').appendChild(jobDiv)
+       // debugger
         this.all.forEach(job => job.render())
         jobDiv.addEventListener('click', this.handleCardClick)
         // (event) => {
         //     this.handleCardClick(event)
         // })
     }
-
-    
-
 
 
     static getJobs() {
