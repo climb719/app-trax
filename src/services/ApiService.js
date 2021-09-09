7f6e671442a944e96379afc3133bc352d19f4087
climb719 <mary.oldweiler@gmail.com>
2021-09-09 10:04:26 -0400

    add fetch with POST for uploading JSON data

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -7,4 +7,19 @@ class ApiService {
    // getJobs = () => fetch(this.api + "/jobs").then(resp => resp.json())
 
     getStatuses = () => fetch(this.api + "/statuses").then(resp => resp.json())
+
+    createJobApp = (newApp) => fetch(this.api + "/jobs", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newApp),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+    //   .catch((error) => {
+    //     console.error('Error:', error);
+    //   });
 }
