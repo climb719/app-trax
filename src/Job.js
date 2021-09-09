3a5f562e329e54a027919d51ab35a852d603a8c8
climb719 <mary.oldweiler@gmail.com>
2021-09-09 10:00:45 -0400

    add event listener for submit form, start handleCreate

--- src/Job.js
+++ src/Job.js
@@ -65,17 +65,14 @@ class Job {
       }
     }
 
-
-
    static handleJobForm = () => {
     modal.open()
     const form = document.getElementById("modal-text")
-
     form.innerHTML = `
-      <h2> Add A New Job App</h2>
-      Title: <input type="text" name="title" required="true">
-      Company: <input type="text" name="company" required="true">
-      Select your application status:<select name="statusName" id="statusId">
+      <h2> Add A New Job App</h2> <form id="create-form">
+      Title: <input type="text" name="title">
+      Company: <input type="text" name="company">
+      Select your application status:<select name="status" id="status_id">
       <option></option>
       <option value="apply">To Apply</option>
       <option value="applied">Applied</option>
@@ -83,17 +80,33 @@ class Job {
       <option value="next">Next Round</option>
       <option value="offer">Offer</option>
       </select>
-      Date:<input type="date" name="date" required="true"><br>
+      Date:<input type="date" name="date"><br>
       <br>
       My notes about the job or application process:<br>
-      <textarea rows="15" cols ="50" required="true" name="notes"></textarea><br>
+      <textarea rows="15" cols ="50" name="notes"></textarea><br>
       <br>
-      Link to App Posting:<input type="text" name="link" required="true"> <br>
+      Link to App Posting:<input type="text" name="link" size="70"><br>
       <br>
       <input type="submit" value="Add My Job App">
       </form>`
     document.getElementById("close").addEventListener('click', e => {
     modal.close() })
+    document.getElementById("create-form").addEventListener("submit", this.handleCreate)
    }
+
+   static handleCreate = (e) => {
+    e.preventDefault()
+    const newApp = {
+      title: e.target.title.value,
+      company: e.target.company.value,
+      status: e.target.status.value,
+      date: e.target.date.value,
+      notes: e.target.notes.value,
+      link: e.target.link.value
+   }
+   console.log(newApp)
+  }
+
+
 
 }
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -4,7 +4,7 @@ class ApiService {
         this.api = api
     }
 
-    getJobs = () => fetch(this.api + "/jobs").then(resp => resp.json())
+   // getJobs = () => fetch(this.api + "/jobs").then(resp => resp.json())
 
     getStatuses = () => fetch(this.api + "/statuses").then(resp => resp.json())
 }
