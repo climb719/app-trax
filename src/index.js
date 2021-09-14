e7991e556ff753b04afd0dfd06915ff7b7de40c4
climb719 <mary.oldweiler@gmail.com>
2021-09-14 09:29:17 -0400

    start user form and test submit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const api = new ApiService("http://localhost:3000")
 const modal = new Modal()
 
 // api.getJobs().then(console.log)
-Status.getStatuses()
+//Status.getStatuses()
 //Job.getJobs()
+
+document.querySelector("form").addEventListener("submit", handleUserSubmit)
+
+function handleUserSubmit(e){
+    e.preventDefault()
+    console.log(e.target.name.value)
+    console.log(e.target.email.value)
+}
 
