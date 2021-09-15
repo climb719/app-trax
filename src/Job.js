282879dc58b4fd612aef33062b8995b75c1225ac
climb719 <mary.oldweiler@gmail.com>
2021-09-15 11:28:38 -0400

    testing and debugging

--- src/Job.js
+++ src/Job.js
@@ -26,6 +26,17 @@ class Job {
       <p class="date">${appDate}</p>
       </div>`
   }
+  
+  
+  static handleCardClick = (e) => {
+    //console.log(Status.all)
+      if (e.target.classList.contains("title")) {
+      const id = e.target.closest(".job-card").dataset.jobId
+      Status.findJob(id)
+       //console.log(job)
+      // job.showDetails()
+     }
+   }
 
   showDetails = () => {
     const {title, company, notes, appDate, link, statusName, id, statusId} = this
@@ -47,18 +58,18 @@ class Job {
 
   handleShowClick = (e) => {
     if (e.target.id == "back") {
-      Status.renderMain() 
+      Status.checkJobArray()
     }  
     else if (e.target.innerText == "Edit") {
-      console.log("edit me!!")
+     // console.log("edit me!!")
         this.renderEdit(e.target)
     }
     else if (e.target.innerText == "Save") {
-      console.log("save me!!")
+     // console.log("save me!!")
      Status.saveUpdate(e.target)
     }
     else if (e.target.id == "delete") {
-        console.log("delete me!!")
+       // console.log("delete me!!")
         if (confirm("Are you sure you want to delete this job?")) {
           this.deleteJob(e.target) }
     }
@@ -100,50 +111,5 @@ class Job {
    })
   }
 
-  static handleJobForm = () => {
-    modal.open()
-    const form = document.getElementById("modal-text")
-    form.innerHTML = `
-      <h2> Add A New Job App</h2> <form id="create-form">
-      Title: <input type="text" name="title" >
-      Company: <input type="text" name="company">
-      Select your application status:<select name="status" id="status_id">
-      <option></option>
-      <option value=1>To Apply</option>
-      <option value=2>Applied</option>
-      <option value=3>Phone Inteview</option>
-      <option value=4>Next Round</option>
-      <option value=5>Offer</option>
-      </select>
-      Date:<input type="date" name="date"><br>
-      <br>
-      My notes about the job or application process:<br>
-      <textarea rows="15" cols ="50" name="notes"></textarea><br>
-      <br>
-      Link to App Posting:<input type="text" name="link" size="70"><br>
-      <br>
-      <input type="submit" value="Add My Job App">
-      </form>`
-    document.getElementById("close").addEventListener('click', e => {
-    modal.close() })
-    document.getElementById("create-form").addEventListener("submit", this.handleCreate)
- }
-
-  static handleCreate = (e) => {
-    e.preventDefault()
-    const newApp = {
-    title: e.target.title.value,
-    company: e.target.company.value,
-    status_id: e.target.status.value,
-    date: e.target.date.value,
-    notes: e.target.notes.value,
-    link: e.target.link.value
-    }
-    api.createJobApp(newApp).then(job => {
-    new Job(job).render()
-    })
-    e.target.reset()
-    modal.close()
-  }
-
+ 
 }
--- src/Status.js
+++ src/Status.js
@@ -1,4 +1,4 @@
-  class Status {
+class Status {
 
     static all = []
 
@@ -6,8 +6,7 @@
         this.id = id
         this.name = name 
         this.jobs = jobs.map(job => new Job(job))
-        this.constructor.all.push(this)
-      
+        this.constructor.all.push(this)  
     }
 
     static getStatuses() {
@@ -30,7 +29,7 @@
         addJob.innerText = "Add a New Job"
         main.append(jobContainer, addJob)
         addJob.addEventListener('click', Job.handleJobForm)
-        jobContainer.addEventListener("click", this.handleCardClick)
+        jobContainer.addEventListener("click", Job.handleCardClick)
         const statusDiv = document.createElement('div')
         jobContainer.appendChild(statusDiv)
         this.all.forEach(status => { 
@@ -49,25 +48,64 @@
         })
     }
 
-    static findJob = (id) => {
-     // console.log(id)
-        this.all.forEach(status => { 
-         const jobClicked = status.jobs.find(job => job.id == id)
-        if (jobClicked) {
-        jobClicked.showDetails()
-        }
-    })
-}
-
-    static handleCardClick = (e) => {
-    // console.log(Status.all)
-        if (e.target.classList.contains("title")) {
-        const id = e.target.closest(".job-card").dataset.jobId
-        this.findJob(id)
-       //console.log(job)
-      // job.showDetails()
+    static handleJobForm = () => {
+        modal.open()
+        const form = document.getElementById("modal-text")
+        form.innerHTML = `
+          <h2> Add A New Job App</h2> <form id="create-form">
+          Title: <input type="text" name="title" >
+          Company: <input type="text" name="company">
+          Select your application status:<select name="status" id="status_id">
+          <option></option>
+          <option value=1>To Apply</option>
+          <option value=2>Applied</option>
+          <option value=3>Phone Inteview</option>
+          <option value=4>Next Round</option>
+          <option value=5>Offer</option>
+          </select>
+          Date:<input type="date" name="date"><br>
+          <br>
+          My notes about the job or application process:<br>
+          <textarea rows="15" cols ="50" name="notes"></textarea><br>
+          <br>
+          Link to App Posting:<input type="text" name="link" size="70"><br>
+          <br>
+          <input type="submit" value="Add My Job App">
+          </form>`
+        document.getElementById("close").addEventListener('click', e => {
+        modal.close() })
+        document.getElementById("create-form").addEventListener("submit", this.handleCreate)
      }
-   }
+    
+    static handleCreate = (e) => {
+        e.preventDefault()
+        const newApp = {
+        title: e.target.title.value,
+        company: e.target.company.value,
+        status_id: e.target.status.value,
+        date: e.target.date.value,
+        notes: e.target.notes.value,
+        link: e.target.link.value
+        }
+        api.createJobApp(newApp).then(job => {
+        new Job(job).render()
+        })
+        e.target.reset()
+        modal.close()
+      }
+    
+
+    
+
+//     static handleCardClick = (e) => {
+//     // console.log(Status.all)
+//         if (e.target.classList.contains("title")) {
+//         const id = e.target.closest(".job-card").dataset.jobId
+//         this.findJob(id)
+//        //console.log(job)
+//       // job.showDetails()
+//      }
+//    }
 
     static saveUpdate = (saveBtn) => {
         const div = saveBtn.closest('Div')
@@ -75,29 +113,60 @@
         const statusEdit = div.querySelector(".edited-status").value
         const id = div.querySelector("#job-id").value
    // debugger
-        const updatedJob = {
+        const oldJob = {
         id: id,
         status_id: statusEdit,
         notes: noteEdit 
         }
-        api.updateJobApp(updatedJob).then(updatedJob => {
-        new Job(updatedJob).showDetails()
-        console.log(updatedJob.id)
-        console.log(updatedJob.statusId) 
-        this.updateJobArray(updatedJob.id, updatedJob.statusId)
+        api.updateJobApp(oldJob).then(oldJob => {
+        new Job(oldJob).showDetails()
+        // console.log(oldJob.id)
+        // console.log(oldJob.statusId) 
+        this.checkJobArray(oldJob.id, oldJob.statusId)
         // find id of updated job and replace job in original array with updated job
     })   
   }
+
+  static findJob = (id) => {
+    // console.log(id)
+       this.all.forEach(status => { 
+        const jobClicked = status.jobs.find(job => job.id == id)
+       if (jobClicked) {
+       jobClicked.showDetails()
+       }
+   })
+}
       
 
-    static updateJobArray(id, statusId) {
-        console.log(id)
-        console.log(statusId)
-        // const status = this.all.find(status => status.id == statusId)
-        // console.log(status)   
+    static checkJobArray(id, statusId) {
+        // console.log(id)
+        // console.log(statusId)
+        // const jobStatus = this.all.find(status => status.id == statusId)
+        // console.log (jobStatus)
+        const result =  this.all.forEach(status => {
+            status.jobs.forEach(job => {
+              if (job.userId == user.id) {
+            const oldJob = status.jobs.find(job => job.id == id) 
+            console.log(oldJob) }
+            //console.log(status)
+              })
+        //     status.map(x => {
+        //     const obj = oldJob.find(({ id }) => id === x.id)
+        //     return obj ? obj : x
+        //   })
+        //   console.log(result)
+        })
     }
 
+        // const jobFound = jobStatus.jobs.find(job => job.id == id)
+        // console.log(jobFound)
+
+        // const status = this.all.find(status => status.id == statusId)
+        // console.log(status) 
+       // this.renderMain()  
 }
+
+
 
 
 
